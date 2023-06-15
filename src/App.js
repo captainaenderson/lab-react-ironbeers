4b6c2b1cdc87cc3a150e6d371ab72ded13d201f2
captainaenderson <annkathrin.wohlrab@gmail.com>
2023-06-15 18:24:03 +0200

    i cannot display anything and i dont know where the mistake is

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Routes, Route} from "react-router-dom";
+import Beers from './pages/Beers';
+import Home from './pages/Home';
+import NewBeer from './pages/NewBeer';
+import RandomBeer from './pages/RandomBeer';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/beers" element={<Beers />} />
+        <Route path="/randombeer" element={<RandomBeer />} />
+        <Route path="/newbeer" element={<NewBeer />} />
+      </Routes>
+      </div>
   );
+  
 }
 
 export default App;
